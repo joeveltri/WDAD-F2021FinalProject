27810aa987f9859b740d723845e6f5ae2baa2c04
joeveltri <57503263+joeveltri@users.noreply.github.com>
2021-12-07 11:23:51 -0500

    Add files via upload

---
+++ src/components/InputSearch.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container style="max-width: 512px">
+    <v-form @submit.stop.prevent="onSearchLocationData">
+      <v-row class="align-start">
+        <v-text-field
+          class="title pa-1"
+          label="Please Enter Location:"
+          v-model="location"
+        ></v-text-field>
+        <v-btn class="ma-3" @click="onSearchLocationData"
+          ><v-icon>mdi-magnify</v-icon></v-btn
+        >
+      </v-row>
+    </v-form>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'InputSearch',
+  data() {
+    return {
+      location: ''
+    };
+  },
+  methods: {
+    onSearchLocationData() {
+      this.$emit('search-location-data', this.location);
+    }
+  }
+};
+</script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav>
+    <v-app-bar app flat>
+      <v-toolbar-items>
+        <v-btn text>
+          <v-icon left large>mdi-cloud-search</v-icon>
+          <v-toolbar-title class="headline ml-4">
+            <span class="font-weight-bold">Weather </span>
+            <span>App</span>
+          </v-toolbar-title>
+        </v-btn>
+      </v-toolbar-items>
+      <v-spacer></v-spacer>
+      <v-btn depressed fab class="grey darken-3" @click="openRepo">
+        <v-icon x-large>mdi-github-circle</v-icon>
+      </v-btn>
+    </v-app-bar>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+  methods: {
+    openRepo() {
+      window.open('https://github.com/joeveltri/WDAD-F2021-FInalProject', '⏎········⏎········_blank⏎······⏎······');
+    }
+  }
+};
+</script>
